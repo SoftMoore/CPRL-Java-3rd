e253d4da0d2451c6cedb5b063591597b5ad61b4b
SoftMoore <jmoore@softmoore.com>
2023-05-25 12:00:15 -0400

    added new test program for strings and handouts on creating multimodule projects

---
+++ Project/examples/Correct/Strings/Correct_411.asm
@@ -0,0 +1,89 @@
+   CALL _main
+   HALT
+_main:
+   PROC 104
+   LDLADDR 8
+   LDCSTR "before tab \t after tab"
+   STORE 48
+   LDLADDR 8
+   LOAD 104
+   PUTSTR 50
+   PUTEOL
+   LDCSTR "length of testString = "
+   PUTSTR 23
+   LDLADDR 8
+   LOADW
+   PUTINT
+   PUTEOL
+   PUTEOL
+   LDLADDR 8
+   LDCSTR "before carriage return \t after carriage return"
+   STORE 96
+   LDLADDR 8
+   LOAD 104
+   PUTSTR 50
+   PUTEOL
+   LDCSTR "length of testString = "
+   PUTSTR 23
+   LDLADDR 8
+   LOADW
+   PUTINT
+   PUTEOL
+   PUTEOL
+   LDLADDR 8
+   LDCSTR "before newline \n after newline"
+   STORE 64
+   LDLADDR 8
+   LOAD 104
+   PUTSTR 50
+   PUTEOL
+   LDCSTR "length of testString = "
+   PUTSTR 23
+   LDLADDR 8
+   LOADW
+   PUTINT
+   PUTEOL
+   PUTEOL
+   LDLADDR 8
+   LDCSTR "before single quote \' after single quote"
+   STORE 84
+   LDLADDR 8
+   LOAD 104
+   PUTSTR 50
+   PUTEOL
+   LDCSTR "length of testString = "
+   PUTSTR 23
+   LDLADDR 8
+   LOADW
+   PUTINT
+   PUTEOL
+   PUTEOL
+   LDLADDR 8
+   LDCSTR "before double quote \" after double quote"
+   STORE 84
+   LDLADDR 8
+   LOAD 104
+   PUTSTR 50
+   PUTEOL
+   LDCSTR "length of testString = "
+   PUTSTR 23
+   LDLADDR 8
+   LOADW
+   PUTINT
+   PUTEOL
+   PUTEOL
+   LDLADDR 8
+   LDCSTR "before backslash \\ after backslash"
+   STORE 72
+   LDLADDR 8
+   LOAD 104
+   PUTSTR 50
+   PUTEOL
+   LDCSTR "length of testString = "
+   PUTSTR 23
+   LDLADDR 8
+   LOADW
+   PUTINT
+   PUTEOL
+   PUTEOL
+   RET 0
